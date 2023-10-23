420301ca8385305ca073ec17f07b1471a417d70c
Alex Keen <iapk2727@gmail.com>
2023-10-23 14:32:02 -0500

    Base Push
    -NoClip is now toggleable during prep phase using RB or "n"
    -NoClip turns off when not in prep or in "kitchen" scene

--- NoClip/Mod.cs
+++ NoClip/Mod.cs
@@ -1,40 +1,67 @@
 ﻿using Kitchen;
 using KitchenMods;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-namespace NoClip
+namespace NoClipPrepToggle
 {
     public class NoClipSystem : GenericSystemBase, IModSystem {
         private const int LayerPlayers = 12;
         private const int LayerStatics = 9;
         private const int LayerDefault = 0;
-        private bool _hasBeenPrepTime = false;
+        private bool PrevPrepTime = false;
 
-        //protected override void Initialise()
-        //{
-        //    base.Initialise();
+        private bool noClipPref = true;
+        private SceneType prevScene = SceneType.Null;
 
-        //    // print layer names
-        //    for (int i = 0; i < 16; i++)
-        //    {
-        //        Debug.Log($"NoClip Mod: Layer {i}: {LayerMask.LayerToName(i)}");
-        //    }
-        //}
-
-        private void OnPrepTimeSwitch(bool isNowPrepTime)
+        //Toggles NoClip based on if the current scene is "kitchen", if it is "prep time", and the players NoClip preference
+        private void ToggleNoClip()
         {
-            // ignore collision only in prep phase
-            // turn collision (back) on if not prep phase
-            Debug.Log($"NoClip Mod: NoClip is now {(isNowPrepTime ? "on" : "off")}.");
-            Physics.IgnoreLayerCollision(LayerPlayers, LayerDefault, isNowPrepTime);
-            Physics.IgnoreLayerCollision(LayerPlayers, LayerStatics, isNowPrepTime);
+            //When not in the "Kitchen" scene NoClip is disabled
+            if(GameInfo.CurrentScene != SceneType.Kitchen)
+            {
+                Physics.IgnoreLayerCollision(LayerPlayers, LayerDefault, false);
+                Physics.IgnoreLayerCollision(LayerPlayers, LayerStatics, false);
+                return;//ignores rest
+            }
+
+            //Everything here and bellow assumes scene is kitchen
+            //If prep time, NoClip based on pref
+            if(GameInfo.IsPreparationTime)
+            {
+                Physics.IgnoreLayerCollision(LayerPlayers, LayerDefault, noClipPref);
+                Physics.IgnoreLayerCollision(LayerPlayers, LayerStatics, noClipPref);
+                Debug.Log($"NoClipPrepToggle Mod: NoClip is now {(noClipPref ? "on" : "off")}.");
+            }
+            //if not prep time or correct scene, noClip disabled
+            else
+            {
+                Physics.IgnoreLayerCollision(LayerPlayers, LayerDefault, false);
+                Physics.IgnoreLayerCollision(LayerPlayers, LayerStatics, false);
+                Debug.Log("NoClipPrepToggle Mod: NoClip is now off.");
+            }
         }
 
         protected override void OnUpdate() {
-            if (GameInfo.IsPreparationTime != _hasBeenPrepTime)
+            //When prep time swaps
+            if (GameInfo.IsPreparationTime != PrevPrepTime)
             {
-                _hasBeenPrepTime = !_hasBeenPrepTime;
-                OnPrepTimeSwitch(_hasBeenPrepTime);
+                PrevPrepTime = GameInfo.IsPreparationTime;
+                ToggleNoClip();
+            }
+
+            //When we leave/ rejoin the kitchen scene noClip corrections need to be made
+            if(GameInfo.CurrentScene != prevScene)
+            {
+                ToggleNoClip();
+                prevScene = GameInfo.CurrentScene;
+            }
+
+            //Checks for input from player to toggle noClip (only changeable while in prep)
+            if ((Gamepad.current.rightShoulder.wasPressedThisFrame || Input.GetKeyDown("n")) && GameInfo.IsPreparationTime == true && GameInfo.CurrentScene == SceneType.Kitchen)
+            {
+                noClipPref = !noClipPref;
+                ToggleNoClip();
             }
         }
     }
